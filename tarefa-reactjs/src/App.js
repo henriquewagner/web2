fb7fdd362355d79e195136c81942a49bfec4daf6
Henrique Bernard Wagner <106853248+henriquewagner@users.noreply.github.com>
2022-10-27 21:56:15 -0300

    aula nova

--- tarefa-reactjs/src/App.js
+++ tarefa-reactjs/src/App.js
@@ -4,39 +4,45 @@ import './App.css';
 
 function App() {
 
-  const [codigo, setCodigo] = useState();
+  const [tarefa, setTarefa] = useState();
   const [descricao, setDescricao] = useState('');
   const [listaTarefa, setListaTarefa] = useState([]);
+
+  
 
   useEffect(() => {
     buscar();
   }, []);
 
+
   function buscar() {
     axios.get('http://localhost:3100/tarefa').then(resultado => {
-      console.log(resultado.data);
       setListaTarefa(resultado.data);
     });
   }
-
+  
 
   function salvar(event) {
     event.preventDefault();
+
+
     let tarefa = {
       codigo: codigo,
       descricao: descricao
     };
-    console.log('tarefa', tarefa);
 
     axios.put('http://localhost:3100/tarefa', tarefa).then(() => {
       buscar();
-    })
+
+      setCodigo();
+      setDescricao('');
+    });
   }
 
   function excluir(tarefa) {
     axios.delete(`http://localhost:3100/tarefa/${tarefa.codigo}`).then((result) => {
       buscar();
-    })
+    });
   }
 
   function editar(tarefa) {
@@ -47,28 +53,29 @@ function App() {
     });
   }
 
+
   return (
-    <div className='container'>
+    <div className="container">
 
       <form onSubmit={(event) => salvar(event)}>
         <div className="mb-3">
-          <label className="from-label">Descrição</label>
+          <label className="form-label">Descrição</label>
           <input type="text" className="form-control"
             value={descricao}
-            onChange={(event) => setDescricao(event.target.value)}></input>
+            onChange={(event) => setDescricao(event.target.value)} />
         </div>
 
         <button type="submit" className="btn btn-primary">Salvar</button>
 
       </form>
 
-      <h3>PABLO LINDO</h3>
+      <h3>Lista de Tarefa</h3>
 
-      <table className='table'>
+      <table className="table">
         <thead>
           <tr>
             <td>Tarefa</td>
-            <td>...</td>
+            <td> </td>
           </tr>
         </thead>
         <tbody>
@@ -77,17 +84,20 @@ function App() {
               <tr key={index}>
                 <td>{tarefa.descricao}</td>
                 <td>
-                <button type="button" className="btn btn-primary" onClick={(event) => editar(tarefa)}>Editar</button>
-                <button type="button" className="btn btn-primary" onClick={(event) => excluir(tarefa)}>Excluir</button>
+                  <button type="button" className="btn btn-link" 
+                    onClick={(event) => editar(tarefa)}>[editar]</button>
+
+                  <button type="button" className="btn btn-link" 
+                    onClick={(event) => excluir(tarefa)} >[excluir]</button>
                 </td>
               </tr>
             ))
           }
+
         </tbody>
       </table>
     </div>
   );
 }
-
 
 export default App;
